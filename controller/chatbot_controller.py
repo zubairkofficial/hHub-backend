a1a43497e28879b2874335057f65115e61a8ed43
Zubair Khan <mzubairkhan.official@gmail.com>
2025-05-27 18:25:42 +0500

    changes

--- controller/chatbot_controller.py
+++ controller/chatbot_controller.py
@@ -44,9 +44,7 @@ async def chatbot_query(request: ChatbotQueryRequest):
     except Exception as e:
         print("Exception occurred in chatbot_query")
         traceback.print_exc()
-        raise HTTPException(status_code=500, detail=f"Chatbot error: {str(e)}") 
-    
-
+        raise HTTPException(status_code=500, detail=f"Chatbot error: {str(e)}")
 
 
 
